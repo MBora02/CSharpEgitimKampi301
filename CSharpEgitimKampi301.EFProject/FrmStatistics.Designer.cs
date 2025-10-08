5a89a5b8ed8429e6927497ebbf560b9a2d583bf6
Mahmut Bora Cihangir <m.bora.cihangir@gmail.com>
2025-10-08 22:11:31 +0300

    EF statistic examples

---
+++ CSharpEgitimKampi301.EFProject/FrmStatistics.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace CSharpEgitimKampi301.EFProject
+{
+    partial class FrmStatistics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblLocationCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblSumCapacity = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblGuideCount = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblAverageCapacity = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.lblLastCountryName = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.lblAvgLocationPrice = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(92)))), ((int)(((byte)(101)))));
+            this.panel1.Controls.Add(this.lblLocationCount);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(51, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(216, 121);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblLocationCount
+            // 
+            this.lblLocationCount.AutoSize = true;
+            this.lblLocationCount.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocationCount.Location = new System.Drawing.Point(83, 64);
+            this.lblLocationCount.Name = "lblLocationCount";
+            this.lblLocationCount.Size = new System.Drawing.Size(37, 29);
+            this.lblLocationCount.TabIndex = 2;
+            this.lblLocationCount.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lokasyon Sayısı";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(150)))), ((int)(((byte)(68)))));
+            this.panel2.Controls.Add(this.lblSumCapacity);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(345, 34);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(216, 121);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblSumCapacity
+            // 
+            this.lblSumCapacity.AutoSize = true;
+            this.lblSumCapacity.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSumCapacity.Location = new System.Drawing.Point(83, 64);
+            this.lblSumCapacity.Name = "lblSumCapacity";
+            this.lblSumCapacity.Size = new System.Drawing.Size(37, 29);
+            this.lblSumCapacity.TabIndex = 2;
+            this.lblSumCapacity.Text = "00";
+            this.lblSumCapacity.Click += new System.EventHandler(this.lblSumCapacity_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(150)))), ((int)(((byte)(68)))));
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(48, 34);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(116, 19);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Toplam Kapasite";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(211)))), ((int)(((byte)(48)))));
+            this.panel3.Controls.Add(this.lblGuideCount);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(628, 34);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(216, 121);
+            this.panel3.TabIndex = 3;
+            // 
+            // lblGuideCount
+            // 
+            this.lblGuideCount.AutoSize = true;
+            this.lblGuideCount.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuideCount.Location = new System.Drawing.Point(93, 64);
+            this.lblGuideCount.Name = "lblGuideCount";
+            this.lblGuideCount.Size = new System.Drawing.Size(37, 29);
+            this.lblGuideCount.TabIndex = 2;
+            this.lblGuideCount.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(62, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 19);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Rehber Sayısı";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(222)))), ((int)(((byte)(129)))));
+            this.panel4.Controls.Add(this.lblAverageCapacity);
+            this.panel4.Controls.Add(this.label8);
+            this.panel4.Location = new System.Drawing.Point(628, 188);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(216, 121);
+            this.panel4.TabIndex = 3;
+            // 
+            // lblAverageCapacity
+            // 
+            this.lblAverageCapacity.AutoSize = true;
+            this.lblAverageCapacity.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverageCapacity.Location = new System.Drawing.Point(83, 64);
+            this.lblAverageCapacity.Name = "lblAverageCapacity";
+            this.lblAverageCapacity.Size = new System.Drawing.Size(37, 29);
+            this.lblAverageCapacity.TabIndex = 2;
+            this.lblAverageCapacity.Text = "00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(48, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 19);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Ortalama Kapasite";
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(101)))), ((int)(((byte)(132)))));
+            this.panel8.Controls.Add(this.lblLastCountryName);
+            this.panel8.Controls.Add(this.label16);
+            this.panel8.Location = new System.Drawing.Point(345, 188);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(216, 121);
+            this.panel8.TabIndex = 3;
+            // 
+            // lblLastCountryName
+            // 
+            this.lblLastCountryName.AutoSize = true;
+            this.lblLastCountryName.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastCountryName.Location = new System.Drawing.Point(68, 64);
+            this.lblLastCountryName.Name = "lblLastCountryName";
+            this.lblLastCountryName.Size = new System.Drawing.Size(37, 29);
+            this.lblLastCountryName.TabIndex = 2;
+            this.lblLastCountryName.Text = "00";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(48, 34);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(119, 19);
+            this.label16.TabIndex = 1;
+            this.label16.Text = "Eklenen Son Ülke";
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(59)))), ((int)(((byte)(90)))));
+            this.panel9.Controls.Add(this.lblAvgLocationPrice);
+            this.panel9.Controls.Add(this.label18);
+            this.panel9.Location = new System.Drawing.Point(51, 188);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(216, 121);
+            this.panel9.TabIndex = 3;
+            // 
+            // lblAvgLocationPrice
+            // 
+            this.lblAvgLocationPrice.AutoSize = true;
+            this.lblAvgLocationPrice.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAvgLocationPrice.Location = new System.Drawing.Point(47, 64);
+            this.lblAvgLocationPrice.Name = "lblAvgLocationPrice";
+            this.lblAvgLocationPrice.Size = new System.Drawing.Size(37, 29);
+            this.lblAvgLocationPrice.TabIndex = 2;
+            this.lblAvgLocationPrice.Text = "00";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(38, 34);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(133, 19);
+            this.label18.TabIndex = 1;
+            this.label18.Text = "Ortalama Tur Fiyatı";
+            // 
+            // FrmStatistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 363);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmStatistics";
+            this.Text = "FrmStatistics";
+            this.Load += new System.EventHandler(this.FrmStatistics_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblLocationCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblSumCapacity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblGuideCount;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblAverageCapacity;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Label lblLastCountryName;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Label lblAvgLocationPrice;
+        private System.Windows.Forms.Label label18;
+    }
+}
---
+++ CSharpEgitimKampi301.EFProject/FrmStatistics.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmStatistics : Form
+    {
+        public FrmStatistics()
+        {
+            InitializeComponent();
+        }
+        EgitimKampiEfTravelDbEntities db = new EgitimKampiEfTravelDbEntities();
+        private void FrmStatistics_Load(object sender, EventArgs e)
+        {
+            lblLocationCount.Text = db.Locations.Count().ToString();
+            lblSumCapacity.Text = db.Locations.Sum(x => x.Capacity).ToString();
+            lblGuideCount.Text = db.Guides.Count().ToString();
+            lblAverageCapacity.Text = db.Locations.Average(x => x.Capacity).ToString();
+            lblAvgLocationPrice.Text = db.Locations.Average(x => (double?)x.Price).Value.ToString("0.00") + " ₺";
+            lblLastCountryName.Text = db.Locations.OrderByDescending(x => x.LocationId).Select(y => y.Country).FirstOrDefault();
+
+
+
+        }
+
+        private void lblSumCapacity_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
